2d91f5c2ed8f11f915aab7240e5c6cb309d9854b
Gianluca Saroni <gianlucasaroni@gmail.com>
2026-02-10 11:12:09 +0100

    Initial Commit

---
+++ Onicotech/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Onicotech
+//
+//  Created by Gianluca Saroni on 10/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Onicotech/OnicotechApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnicotechApp.swift
+//  Onicotech
+//
+//  Created by Gianluca Saroni on 10/02/26.
+//
+
+import SwiftUI
+
+@main
+struct OnicotechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
